bc1a227d9650fd0e763b466c342fa2b8f1c449d7
Ayhan <ayhan.mslmv.539@gmail.com>
2026-06-29 12:02:32 +0400

    Clean up code: Move Day 2 code to correct folder

--- day-01/index.js
+++ day-01/index.js
@@ -4,17 +4,3 @@ let name = "Ayhan";
 console.log("Hello", name);
 console.log("Node is working!");
 console.log("Day 2 JavaScript started");
-
-// Variables
-let age = 20;
-let city = "Baku";
-
-console.log(age);
-console.log(city);
-
-// Simple function
-function greet(name) {
-    return "Hello " + name;
-}
-
-console.log(greet("Ayhan"));
