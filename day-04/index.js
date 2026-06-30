818cd826c88a7a7e9923f4ea8713685bb21200a7
Ayhan <ayhan.mslmv.539@gmail.com>
2026-07-01 02:19:43 +0400

    Day 4: Create and manipulate JS arrays

---
+++ day-04/index.js
@@ -0,0 +1,16 @@
+console.log("--- Day 4: Arrays ---");
+
+// 1. Creating an array (list of fruits)
+let fruits = ["Apple", "Banana", "Cherry", "Mango"];
+console.log("My fruits:", fruits);
+
+// 2. Accessing elements by their index (Index starts from 0!)
+console.log("The first fruit is:", fruits[0]);
+console.log("The third fruit is:", fruits[2]);
+
+// 3. Adding a new item to the end of the array
+fruits.push("Orange");
+console.log("After adding Orange:", fruits);
+
+// 4. Checking the length of the array (how many items are inside)
+console.log("Total number of fruits:", fruits.length);
