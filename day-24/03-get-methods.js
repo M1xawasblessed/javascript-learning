b25e30e379127fe94441ef3382d3cda5b40df44f
Ayhan <ayhan.mslmv.539@gmail.com>
2026-07-21 20:32:16 +0400

    Add Day 24 date get methods

---
+++ day-24/03-get-methods.js
@@ -0,0 +1,5 @@
+const date = new Date("2026-07-21");
+console.log("Year:", date.getFullYear());
+console.log("Month:", date.getMonth());
+console.log("Day of the month:", date.getDate());
+console.log("Day of the week:", date.getDay());
