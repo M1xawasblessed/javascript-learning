8840b1e45d5cba8eb101d305a283522cec971d6d
Ayhan <ayhan.mslmv.539@gmail.com>
2026-07-21 20:32:16 +0400

    Add Day 24 specific date creation

---
+++ day-24/02-create-specific-date.js
@@ -0,0 +1,4 @@
+const specificDate = new Date("2026-07-21T18:30:00");
+const fromArguments = new Date(2026, 6, 21, 18, 30, 0); 
+console.log("String parsing:", specificDate);
+console.log("From arguments:", fromArguments);
