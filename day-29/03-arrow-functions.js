3df215a6f692843a6807717efc269bc69b947d9e
Ayhan <ayhan.mslmv.539@gmail.com>
2026-07-26 14:41:00 +0400

    Refactor: convert traditional functions to arrow functions

---
+++ day-29/03-arrow-functions.js
@@ -0,0 +1,7 @@
+// BAD: Verbose function declaration for simple tasks
+// function multiply(a, b) { return a * b; }
+
+// GOOD: Concise arrow function
+const multiply = (a, b) => a * b;
+
+console.log("Multiplication Result:", multiply(4, 5));
